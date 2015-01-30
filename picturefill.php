5649962c4d85ef76ce28372e0b8ba61fb697006f
Jason Britton <jason@needmoredesigns.com>
2015-01-30 09:44:39 -0800

    Update the breakpoints for picturefill.

--- picturefill.php
+++ picturefill.php
@@ -22,12 +22,14 @@ Using an ACF Image
 <?php $mobile = wp_get_attachment_image_src(get_field('featured_image')['id'], 'hero-mobile'); ?>
 <?php $tablet = wp_get_attachment_image_src(get_field('featured_image')['id'], 'hero-tablet'); ?>
 <?php $desktop = wp_get_attachment_image_src(get_field('featured_image')['id'], 'hero-desktop'); ?>
+<?php $retina = wp_get_attachment_image_src(get_field('featured_image')['id'], 'hero02x'); ?>
 
 <picture>
 	<!--[if IE 9]><video style="display: none;"><![endif]-->
-	<source srcset="<?php echo $mobile[0]; ?>" media="(max-width: 600px)">
+	<source srcset="<?php echo $mobile[0]; ?>" media="(max-width: 400px)">
 	<source srcset="<?php echo $tablet[0]; ?>" media="(max-width: 801px)">
-	<source srcset="<?php echo $desktop[0]; ?>" media="(min-width: 802px)">
+	<source srcset="<?php echo $desktop[0]; ?>" media="(max-width: 1024px)">
+	<source srcset="<?php echo $retina[0]; ?>" data-media="(min-device-pixel-ratio: 2)">
 	<!--[if IE 9]></video><![endif]-->
 	<img srcset="<?php echo $large_image[0]; ?>">
 </picture>
