52b7bc4ed3a3ccc8d344f11dbbabfc3030ff3fb1
Jason Britton <velograph@gmail.com>
2015-04-02 16:56:20 -0700

    Tab cleanup.

--- acf_repeater_flexible_content_loops.php
+++ acf_repeater_flexible_content_loops.php
@@ -15,16 +15,16 @@
 <!-- Flexible Content -->
 <?php if( have_rows('flexible_content_field_name') ) : ?>
 
-  <?php while ( have_rows('flexible_content_field_name') ) : ?>
+    <?php while ( have_rows('flexible_content_field_name') ) : ?>
 
-		<?php the_row(); ?>
+        <?php the_row(); ?>
 
-      <?php if( get_row_layout() == 'paragraph' ) : ?>
+        <?php if( get_row_layout() == 'paragraph' ) : ?>
 
-      	<?php the_sub_field('text'); ?>
+            <?php the_sub_field('text'); ?>
 
-      <?php endif; ?>
+        <?php endif; ?>
 
-  <?php endwhile; ?>
+    <?php endwhile; ?>
 
 <?php endif; ?>
