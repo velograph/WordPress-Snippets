754af280a7d6dbefe9b41028f2d2c0159e8cff29
Jason Britton <velograph@gmail.com>
2015-05-13 10:30:19 -0700

    code cleanup

--- acf_repeater_flexible_content_loops.php
+++ acf_repeater_flexible_content_loops.php
@@ -1,13 +1,13 @@
 <!-- Repeater -->
 <?php if( have_rows('repeater_field_name') ) : ?>
 
-  <?php while ( have_rows('repeater_field_name') ) : ?>
+    <?php while ( have_rows('repeater_field_name') ) : ?>
 
-    <?php the_row(); ?>
+        <?php the_row(); ?>
 
-      <?php the_sub_field('sub_field_name'); ?>
+        <?php the_sub_field('sub_field_name'); ?>
 
-  <?php endwhile; ?>
+    <?php endwhile; ?>
 
 <?php endif; ?>
 
