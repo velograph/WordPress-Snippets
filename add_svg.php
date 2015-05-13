eb13e48350ff920cfaa0f19e0c5fe7bbd0599886
Jason Britton <velograph@gmail.com>
2015-05-13 10:30:07 -0700

    Add svg snippet

---
+++ add_svg.php
@@ -0,0 +1,11 @@
+<?php
+
+// This allows the uploading of SVG files in the media uploader.
+
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+?>
